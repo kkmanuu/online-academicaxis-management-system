62aa271047c9dd5c62007343746e2363355c2bd0
kkmanuu <kipngenoemmanuel479@gmail.com>
2025-07-27 04:33:45 +0300

    feat: Impliment the connection for both fronted and backend

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ examHandler.initialize(server);
 app.use(cors({
   origin: [
     "http://localhost:3000", // For local development
-    "https://academicsystem-g393.onrender.com"
+    "https://academicaxissystem.onrender.com"
   ],
   credentials: true
 }));
