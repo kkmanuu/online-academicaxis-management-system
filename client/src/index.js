568d55a5286e398acae42bcea2943cc2945a0a7a
kkmanuu <kipngenoemmanuel479@gmail.com>
2026-06-15 21:28:48 +0300

    feat(auth): redesign login and register UI using Bootstrap

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
 import theme from './theme';
